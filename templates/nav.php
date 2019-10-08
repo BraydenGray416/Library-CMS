5f041eac218d48beca6482940cc6df0c55de687d
BraydenGray416 <brayden.gray98@gmail.com>
2019-10-09 09:07:27 +1300

    converted to php

---
+++ templates/nav.php
@@ -0,0 +1,26 @@
+<header class="blog-header py-3 bg-info">
+    <div class="container">
+        <div class="row text-center d-flex">
+            <div class="col-6 col-md-3 d-flex align-items-center justify-content-center justify-content-md-start order-2 order-md-1 pt-2 pt-md-0">
+                <a class="text-light" href="#">Subscribe</a>
+            </div>
+            <div class="col-12 col-md order-1 order-md-2">
+                <a class="blog-header-logo text-light" href="index.php">Yoobee School of Design Library</a>
+            </div>
+            <div class="col-6 col-md-3 d-flex align-items-center justify-content-center justify-content-md-end order-3 order-md-3 pt-2 pt-md-0">
+                <a class="text-light" href="#">
+                    <i class="fas fa-search mx-3"></i>
+                </a>
+                <a class="btn btn-sm btn-outline-light" href="#">Sign up</a>
+            </div>
+        </div>
+    </div>
+</header>
+
+<div class="container">
+    <div class="nav-scroller py-1 mb-2">
+        <nav class="nav d-flex justify-content-around">
+            <a class="p-2 text-muted" href="allBooks.php">Books</a>
+            <a class="p-2 text-muted" href="">Movies</a>
+        </nav>
+    </div>
