22299178b7368a2dbe29d26a7a6f7cbf5e10cf46
BraydenGray416 <brayden.gray98@gmail.com>
2019-10-14 09:13:25 +1300

    all books can be viewed as well as ability to select single book

--- books/addBook.php
+++ books/addBook.php
@@ -1,69 +1,63 @@
 <?php
     require('../templates/header.php');
-
-
-    if ($_POST) {
+    if($_POST){
         // var_dump($_POST);
         // var_dump('you have submitted a form');
-
         extract($_POST);
         // var_dump($title);
         // var_dump($author);
         // var_dump($description);
-
         $errors = array();
-
-        if (empty($title)) {
+        if(empty($title)){
             // var_dump('the title is empty');
-            array_push($errors, 'The title is empty, please add a value');
-        } elseif (strlen($title) < 5) {
+            array_push($errors, 'The title is empty, please add a value.');
+        } else if(strlen($title) < 5 ){
             // var_dump('the length must be at least 5');
-            array_push($errors, 'The title must be at least 5 characters');
-        } elseif (strlen($title) > 100) {
-            // var_dump('the length must be less than 100');
-            array_push($errors, 'The title must less than 100 characters');
+            array_push($errors, 'The title length must be at least 5 characters.');
+        } else if(strlen($title) > 100){
+            // var_dump('the length must less than 100');
+            array_push($errors, 'The title length must be no more than 100 characters.');
         }
-
-        if (empty($year)) {
-            array_push($errors, 'The year is empty, please add a value.');
-        } else if(strlen($year) > 4){
-            array_push($errors, 'The year length myst be less than 4 characters.');
+        if(empty($author)){
+            array_push($errors, 'The Author is empty, please add a value.');
+        } else if(strlen($author) < 5 ){
+            array_push($errors, 'The Author length must be at least 5 characters.');
+        } else if(strlen($author) > 100){
+            array_push($errors, 'The Author length must be no more than 100 characters.');
         }
-
-        if (empty($author)) {
-            // var_dump('the author is empty');
-            array_push($errors, 'The author is empty, please add a value');
-        } elseif (strlen($author) < 5) {
-            // var_dump('the length must be at least 5');
-            array_push($errors, 'The author must be at least 5 characters');
-        } elseif (strlen($author) > 100) {
-            // var_dump('the length must be less than 100');
-            array_push($errors, 'The author must less than 100 characters');
+        if(empty($year)){
+            array_push($errors, 'The Year is empty, please add a value.');
+        } else if(strlen($year) > 4 ){
+            array_push($errors, 'The Year length must be less than 4 characters.');
         }
-
-        if (empty($description)) {
-            // var_dump('the description is empty');
-            array_push($errors, 'The description is empty, please add a value');
-        } elseif (strlen($description) < 10) {
-            // var_dump('the length must be at least 10');
-            array_push($errors, 'The description must be at least 10 characters');
-        } elseif (strlen($description) > 65535) {
-            var_dump('the length must be less than 65535');
-            array_push($errors, 'The description must less than 65,535 characters');
+        if(empty($description)){
+            array_push($errors, 'The Description is empty, please add a value.');
+        } else if(strlen($description) < 10 ){
+            array_push($errors, 'The Description length must be at least 10 characters.');
+        } else if(strlen($description) > 65535){
+            array_push($errors, 'The title length must be no more than 65535 characters.');
         }
-    }
-
-    if (empty($errors)) {
-        $safeTitle = mysqli_real_escape_string($dbc, $title);
-        $safeAuthor = mysqli_real_escape_string($dbc, $author);
-        $safeDescription = mysqli_real_escape_string($dbc, $description);
-
-        $sql = "INSERT INTO `authors`(`name`) VALUES ('$safeAuthor')";
-        $result = mysqli_query($dbc, $sql);
-        if ($result && mysqli_affected_rows($dbc) > 0) {
-            var_dump('author was added');
-        } else {
-            die('Something went wrong with adding in our author');
+        if(empty($errors)){
+            // $title = mysqli_real_escape_string($dbc, $title);
+            $safeTitle = mysqli_real_escape_string($dbc, $title);
+            $safeAuthor = mysqli_real_escape_string($dbc, $author);
+            $safeYear = mysqli_real_escape_string($dbc, $year);
+            $safeDescription = mysqli_real_escape_string($dbc, $description);
+            // $sql = "INSERT INTO `authors`(`name`) VALUES ('$safeAuthor')";
+            // $result = mysqli_query($dbc, $sql);
+            // if($result && mysqli_affected_rows($dbc) > 0){
+            //     $authorID = $dbc->insert_id;
+            // } else {
+            //     die('Something went wrong with adding in our author');
+            // }
+            $authorID = 1;
+            $booksSql = "INSERT INTO `books`( `title`, `year`, `description`, `author_id`) VALUES ('$safeTitle',$safeYear,'$safeDescription',$authorID)";
+            $booksResult = mysqli_query($dbc, $booksSql);
+            if($booksResult && mysqli_affected_rows($dbc) > 0){
+                header('Location: singleBook.php');
+            } else {
+                die('Something went wrong with adding in our books');
+            }
         }
     }
 ?>
@@ -74,12 +68,12 @@
             </div>
         </div>
 
-        <?php if ($_POST && !empty($errors)): ?>
+        <?php if($_POST && !empty($errors)): ?>
             <div class="row mb-2">
                 <div class="col">
                     <div class="alert alert-danger pb-0" role="alert">
                         <ul>
-                            <?php foreach ($errors as $error): ?>
+                            <?php foreach($errors as $error): ?>
                                 <li><?php echo $error; ?></li>
                             <?php endforeach; ?>
                         </ul>
@@ -93,7 +87,7 @@
                 <form action="./books/addBook.php" method="post" enctype="multipart/form-data" autocomplete="off">
                     <div class="form-group">
                       <label for="title">Book Title</label>
-                      <input type="text" class="form-control" name="title"  placeholder="Enter book title" value="<?php if($_POST){echo $title; } ?>">
+                      <input type="text" class="form-control" name="title"  placeholder="Enter book title" value="<?php if($_POST){ echo $title; }; ?>">
                     </div>
 
                     <div class="form-group">
@@ -103,12 +97,12 @@
 
                     <div class="form-group author-group">
                       <label for="author">Author</label>
-                      <input type="text" autocomplete="off" class="form-control"  name="author" placeholder="Enter books author" value="<?php if($_POST){echo $author; } ?>">
+                      <input type="text" autocomplete="off" class="form-control"  name="author" placeholder="Enter books author" value="<?php if($_POST){ echo $author; }; ?>">
                     </div>
 
                     <div class="form-group">
                       <label for="description">Book Description</label>
-                      <textarea class="form-control" name="description" rows="8" cols="80" placeholder="Description about the book"><?php if($_POST){echo $description; }; ?></textarea>
+                      <textarea class="form-control" name="description" rows="8" cols="80" placeholder="Description about the book"><?php if($_POST){ echo $description; }; ?></textarea>
                     </div>
 
                     <div class="form-group">
@@ -121,6 +115,4 @@
             </div>
         </div>
 
-    <?php
-    require('../templates/footer.php');
-    ?>
+<?php require('../templates/footer.php'); ?>
